bf12d0fc3cd69d3f2dd1fa506d1ddc3fce91e335
zzhutianyu <704735206@qq.com>
2017-11-23 13:12:04 +0800

    update footer

--- resources/assets/js/components/footer/footer.vue
+++ resources/assets/js/components/footer/footer.vue
@@ -1,53 +1,66 @@
 <template>
-     <div class="m-container">
-         <footer>
-             <div class="f-con clearfix">
-                 <div class="f-item">
-                     <ul>
-                         <li>个人</li>
-                         <li class="f-qq">
-                             <div class="f-erwei">
-                                 <img src="/assets/weixin.jpg" alt="">
-                             </div>
-                         </li>
-                         <li class="f-weixin">
-                             <div class="f-erwei">
-                                 <img src="/assets/weixin.jpg" alt="">
-                                 <p>关注微信号</p>
-                             </div>
-                         </li>
-                         <li class="f-github">
-                             <a href="https://github.com/zzhutianyu"></a>
-                         </li>
+    <main>
+        <v-content style="background:#27BBF5;">
+            <v-container color="light-blue" >
+                <v-layout row wrap>
+                        <v-flex xs12   sm4 >
+                            <div class="f-item">
+                                <ul>
+                                    <li>个人</li>
+                                    <li class="f-qq">
+                                        <div class="f-erwei">
+                                            <img src="/assets/weixin.jpg" alt="">
+                                        </div>
+                                    </li>
+                                    <li class="f-weixin">
+                                        <div class="f-erwei">
+                                            <img src="/assets/weixin.jpg" alt="">
+                                            <p>关注微信号</p>
+                                        </div>
+                                    </li>
+                                    <li class="f-github">
+                                        <a href="https://github.com/zzhutianyu"></a>
+                                    </li>
 
-                     </ul>
-                     
-                 </div>
-                 <div class="f-item">
-                     <ul>
-                         <li>APP-LIST</li>
-                         <li></li>
-                     </ul>
+                                </ul>
 
-                 </div>
-                 <div class="f-item">
-                     <ul>
-                         <li>友情-链接</li>
-                         <li v-for="item in rolls">
-                             <a :href="item.url">{{ item.name}}</a>
-                         </li>
-                     </ul>
-                 </div>
-             </div>
-         </footer>
-         <div class="copyright">
-             <p class="cr">Copyright © 诸天域 | 浙ICP备17027797号-1</p>
-         </div>
-     </div>
+                            </div>
+                        </v-flex>
+                        <v-flex xs12  sm4 >
+                            <div class="f-item">
+                                <ul>
+                                    <li>APP-LIST</li>
+                                    <li></li>
+                                </ul>
+
+                            </div>
+                        </v-flex>
+                        <v-flex xs12  sm4 >
+                            <div class="f-item">
+                                <ul>
+                                    <li>友情-链接</li>
+                                    <li v-for="item in rolls">
+                                        <a :href="item.url">{{ item.name}}</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </v-flex>
+
+                </v-layout>
+                <v-layout row wrap align-center>
+                <v-flex xs12 copyright>
+                    <p class="cr">Copyright © 诸天域 | 浙ICP备17027797号-1</p>
+               </v-flex>
+                </v-layout>
+            </v-container>
+        </v-content>
+
+    </main>
 </template>
 
 <script>
     import {baseUrl} from '../../config/env'
+
     export default {
         data() {
             return {
@@ -56,7 +69,7 @@
             }
         },
         mounted() {
-          this.getRolls();
+            this.getRolls();
         },
         methods: {
             async getRolls() {
@@ -68,180 +81,163 @@
     }
 </script>
 
-<style lang="scss" >
+<style lang="scss">
     @import "../../style/mixin";
-    .m-container {
-        @include wh(100%, 350px);
-        background-color: #27BBF5;
-        footer {
-            @include wh(100%, 290px);
-            position: relative;
-            .f-con {
-                width: auto;
-                margin: 0 auto;
-                padding: 0 15px;
-                @include center;
-                @include flexType(center);
-                .f-item {
-                    &:nth-child(2n-1) {
-                        border-right: 2px solid #e5e9ef;
-                        border-left: 2px solid #e5e9ef;
-                    }
-                    &:first-child {
-                        .f-qq {
-                            position: relative;
-                            margin: 0 auto;
-                            @include backImg('/assets/qq.png');
-                            @include wh(32px, 32px);
 
-                            &:hover {
-                                @include backImg('/assets/qq1.png');
-                                .f-erwei {
-                                    display: block;
-                                }
-                            }
-                            .f-erwei {
-                                padding: 5px;
-                                position: absolute;
-                                @include wh(160px, 160px);
-                                background-color: #fff;
-                                @include borderRadius(10px);
-                                bottom: 140%;
-                                left: -65px;
+        .bColor {
+            background: #27BBF5;
+        }
+        .f-item {
+            &:nth-child(2n-1) {
+                border-right: 2px solid #e5e9ef;
+                border-left: 2px solid #e5e9ef;
+            }
+            &:first-child {
+                .f-qq {
+                    position: relative;
+                    margin: 0 auto;
+                    @include backImg('/assets/qq.png');
+                    @include wh(32px, 32px);
 
-                                &:before {
-                                    position: absolute;
-                                    content: ' ';
-                                    border-left: .1rem solid transparent;
-                                    border-right:.1rem solid transparent;
-                                    border-top: .1rem solid #fff;
-                                    top: 100%;
-                                    left: 50%;
-                                    transform: translateX(-50%);
-                                }
-                                display: none;
-
-                                img {
-                                    @include wh(150px, 150px);
-                                }
-                            }
-                        }
-                        .f-weixin {
-                            position: relative;
-                            margin: 0 auto;
-                            @include backImg('/assets/weixin.png');
-                            @include wh(32px, 32px);
-                            &:hover {
-                                @include backImg('/assets/weixin1.png');
-                                .f-erwei {
-                                    display: block;
-                                }
-                            }
-
-                            .f-erwei {
-                                position: absolute;
-                                @include wh(160px, 160px);
-                                background-color: #fff;
-                                @include borderRadius(10px);
-                                bottom: 130%;
-                                left: -65px;
-                                display: none;
-
-                                &:before {
-                                    position: absolute;
-                                    content: ' ';
-                                    border-left: .1rem solid transparent;
-                                    border-right:.1rem solid transparent;
-                                    border-top: .1rem solid #fff;
-                                    top: 100%;
-                                    left: 50%;
-                                    transform: translateX(-50%);
-                                }
-
-
-                                img {
-                                    @include wh(140px, 140px)
-                                }
-
-                                p {
-                                    @include fontColor(10px, #888);
-                                    transform: translateY(-.1rem);
-                                }
-
-                            }
-                        }
-
-                        .f-github {
-                            position: relative;
-                            margin: 0 auto;
-                            @include backImg('/assets/github.png');
-                            @include wh(32px, 32px);
-
-                            &:hover {
-                                @include backImg('/assets/github1.png');
-                                .f-erwei {
-                                    display: block;
-                                }
-                            }
-
-                            a {
-                                display: block;
-                                @include wh(32px, 32px);
-                            }
-
+                    &:hover {
+                        @include backImg('/assets/qq1.png');
+                        .f-erwei {
+                            display: block;
                         }
                     }
+                    .f-erwei {
+                        padding: 5px;
+                        position: absolute;
+                        @include wh(160px, 160px);
+                        background-color: #fff;
+                        @include borderRadius(10px);
+                        bottom: 140%;
+                        left: -65px;
 
-                    margin-left: 40px;
-                    @include wh(280px, 280px);
-                    padding: 20px;
-                    ul {
-                        @include wh(250px, auto);
-                        margin: 0 auto;
+                        &:before {
+                            position: absolute;
+                            content: ' ';
+                            border-left: .1rem solid transparent;
+                            border-right: .1rem solid transparent;
+                            border-top: .1rem solid #fff;
+                            top: 100%;
+                            left: 50%;
+                            transform: translateX(-50%);
+                        }
+                        display: none;
 
-                        li {
-                            text-align: center;
-                            @include wh(100%, 25px);
-                            @include fontColor(14px, #fff);
-
-                            &:first-child {
-                                @include fontColor(20px, #fff);
-                                margin-bottom: 20px;
-                            }
-                            a {
-                                @include wh(100%, 25px);
-                                @include fontColor(14px, #888);
-                                text-decoration: none;
-
-                                &:hover {
-                                    text-decoration: underline;
-                                }
-
-                            }
-
+                        img {
+                            @include wh(150px, 150px);
                         }
                     }
                 }
+                .f-weixin {
+                    position: relative;
+                    margin: 0 auto;
+                    @include backImg('/assets/weixin.png');
+                    @include wh(32px, 32px);
+                    &:hover {
+                        @include backImg('/assets/weixin1.png');
+                        .f-erwei {
+                            display: block;
+                        }
+                    }
+
+                    .f-erwei {
+                        position: absolute;
+                        @include wh(160px, 160px);
+                        background-color: #fff;
+                        @include borderRadius(10px);
+                        bottom: 130%;
+                        left: -65px;
+                        display: none;
+
+                        &:before {
+                            position: absolute;
+                            content: ' ';
+                            border-left: .1rem solid transparent;
+                            border-right: .1rem solid transparent;
+                            border-top: .1rem solid #fff;
+                            top: 100%;
+                            left: 50%;
+                            transform: translateX(-50%);
+                        }
+
+                        img {
+                            @include wh(140px, 140px)
+                        }
+
+                        p {
+                            @include fontColor(10px, #888);
+                            transform: translateY(-.1rem);
+                        }
+
+                    }
+                }
+
+                .f-github {
+                    position: relative;
+                    margin: 0 auto;
+                    @include backImg('/assets/github.png');
+                    @include wh(32px, 32px);
+
+                    &:hover {
+                        @include backImg('/assets/github1.png');
+                        .f-erwei {
+                            display: block;
+                        }
+                    }
+
+                    a {
+                        display: block;
+                        @include wh(32px, 32px);
+                    }
+
+                }
             }
 
-            .m-connect {
+            /*margin-left: 40px;*/
+            @include wh(280px, 280px);
+            padding: 20px;
+            ul {
+                @include wh(250px, auto);
+                margin: 0 auto;
 
+                li {
+                    text-align: center;
+                    @include wh(100%, 25px);
+                    @include fontColor(14px, #fff);
 
+                    &:first-child {
+                        @include fontColor(20px, #fff);
+                        margin-bottom: 20px;
+                    }
+                    a {
+                        @include wh(100%, 25px);
+                        @include fontColor(14px, #888);
+                        text-decoration: none;
+
+                        &:hover {
+                            text-decoration: underline;
+                        }
+
+                    }
+
+                }
             }
         }
-        .copyright {
-            @include wh(100%, 60px);
-            background: #27BBF5;
-            line-height: 60px;
-            .cr {
-                margin:0;
-                margin-left: 30%;
-                @include fontColor(16px, #fff);
-            }
-        }
 
+
+    .copyright {
+
+        background: #27BBF5;
+
+        .cr {
+            margin: 0;
+            margin-left: 30%;
+            @include fontColor(16px, #fff);
+        }
     }
-
 
 
 </style>
