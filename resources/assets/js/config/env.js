eff7050d17610109a3a6e5c68e2747b70231a9d9
zzhutianyu <704735206@qq.com>
2017-11-26 12:28:38 +0800

    upate

--- resources/assets/js/config/env.js
+++ resources/assets/js/config/env.js
@@ -1,4 +1,4 @@
 export const baseUrl = 'http://www.zxzflower.net';
 export const grantType = 'password';
-export const clientId = '5a192ab5d65ea81bac001884';
-export const clientSecrte = 'VnzVfZQ30Ktik0OE0iyXO2W9BrAH4ekgmSwjq6fd';
+export const clientId = '5a1a4135d65ea810c8003d44';
+export const clientSecrte = 'G2aW5yvaoOFQEyUrGzjnij2TXhvZOjVcHA65j76b';
